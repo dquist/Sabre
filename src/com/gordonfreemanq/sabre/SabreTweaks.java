984210cad88b13cfe973e297ce85cfe21f7e3884
Gordon <CivFactions@gmail.com>
2016-02-19 09:57:44 -0600

    Add log recipe back

--- src/com/gordonfreemanq/sabre/SabreTweaks.java
+++ src/com/gordonfreemanq/sabre/SabreTweaks.java
@@ -135,6 +135,31 @@ public class SabreTweaks implements Listener {
 		ShapelessRecipe emeraldToExpRecipe = new ShapelessRecipe(new ItemStack(Material.EXP_BOTTLE, 9));
 		emeraldToExpRecipe.addIngredient(Material.EMERALD);
 		Bukkit.addRecipe(emeraldToExpRecipe);
+
+		// Recipe: 4 logs create 1 plank
+		ShapelessRecipe logsToPlank = new ShapelessRecipe(new ItemStack(Material.WOOD, 1, (short)0));
+		logsToPlank.addIngredient(4, Material.LOG, 0);
+		Bukkit.addRecipe(logsToPlank);
+
+		ShapelessRecipe logsToPlank1 = new ShapelessRecipe(new ItemStack(Material.WOOD, 1, (short)1));
+		logsToPlank1.addIngredient(4, Material.LOG, 1);
+		Bukkit.addRecipe(logsToPlank1);
+
+		ShapelessRecipe logsToPlank2 = new ShapelessRecipe(new ItemStack(Material.WOOD, 1, (short)2));
+		logsToPlank2.addIngredient(4, Material.LOG, 2);
+		Bukkit.addRecipe(logsToPlank2);
+
+		ShapelessRecipe logsToPlank3 = new ShapelessRecipe(new ItemStack(Material.WOOD, 1, (short)3));
+		logsToPlank3.addIngredient(4, Material.LOG, 3);
+		Bukkit.addRecipe(logsToPlank3);
+
+		ShapelessRecipe logsToPlank4 = new ShapelessRecipe(new ItemStack(Material.WOOD, 1, (short)4));
+		logsToPlank4.addIngredient(4, Material.LOG_2, 0);
+		Bukkit.addRecipe(logsToPlank4);
+
+		ShapelessRecipe logsToPlank5 = new ShapelessRecipe(new ItemStack(Material.WOOD, 1, (short)5));
+		logsToPlank5.addIngredient(4, Material.LOG_2, 1);
+		Bukkit.addRecipe(logsToPlank5);
 	}
 
 	/**
